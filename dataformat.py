57a8e076099835ab74e9c106366f56c2e76f00f3
Olga Sleptsova <osleptsova2gmail.com>
2020-06-21 18:07:17 +0300

    first commit

--- dataformat.py
+++ dataformat.py
@@ -1,35 +1,33 @@
 import json
 
 from collections import Counter
-def opens(filename):
+def reading (filename):
     with open(filename,encoding = 'utf-8') as f:
         data = json.load(f)
     return data
-def  list_word(filename):
+def  list_news (filename):
     list1 =[]
     for items in filename['rss']['channel']["items"]:
-      list1.append(items['description'].split())
-    return list1
-def tenmax_word(filename):
-    list2 =[]
-    for words in filename:
-        for word in words:
+        for word in items['description'].split():
             if len(word) > 6:
-                list2.append(word)
-    list3 = Counter(list2)
-    sorted_list = sorted(list3.items(),key = lambda kv: kv[1])
+                list1.append(word.capitalize())
+    return list1
+def listtop_ten(filename):
+    list2 = Counter(filename)
+    sorted_list = sorted(list2.items(),key = lambda kv: kv[1])
     sorted_list.reverse()
     print(sorted_list[0:10])
 
 
 
 def top_ten(falename):
-    x = opens(falename)
-    y = list_word(x)
-    z =tenmax_word(y)
+    x = reading(falename)
+    y = list_news(x)
+    z =listtop_ten(y)
     return z
 
 top_ten("newsarf.json")
+
 
 import xml.etree.ElementTree as ET
 def reading (filename):
@@ -44,8 +42,9 @@ def list_news(filename):
     for items in filename:
         for m in items.find("description").text.split():
             if len(m) > 6:
-                list1.append(m)
+                list1.append(m.capitalize())
     return list1
+
 def listtop_ten (filename):
     list2 =Counter(filename)
     sorted_list = sorted(list2.items(), key=lambda kv: kv[1])
@@ -56,5 +55,5 @@ def top_ten(filename):
     z = list_news(x)
     y = listtop_ten(z)
     return y
-
+#
 top_ten("newsarf.xml")
